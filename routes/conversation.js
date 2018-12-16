7ef5899709af6c8435fcb0faa3c04e318c304879
77ganesh <77ganesh.nitt@gmail.com>
2018-12-16 14:13:06 +0530

    init commit

---
+++ routes/conversation.js
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const AssistantV1 = require('watson-developer-cloud/assistant/v1'); // watson sdk
+
+
+/**
+ * Watson SDK automaticly search for the correct enviromental variables in the .env
+ * file i.e. username and password or IAM credentials
+ */
+var assistant = new AssistantV1({
+  version: '2018-07-10'
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+const updateMessage = (input, response) => {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+	console.log(JSON.stringify(response))
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+ 
+  response.output.text = responseText;
+  return response;
+};
+
+
+module.exports = function (app) {
+
+  app.post('/api/message', (req, res, next) => {
+    const workspace = process.env.WORKSPACE_ID || '<workspace-id>';
+    if (!workspace || workspace === '<workspace-id>') {
+      return res.json({
+        output: {
+          text: 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' +
+            '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> ' +
+            'documentation on how to set this variable. <br>' +
+            'Once a workspace has been defined the intents may be imported from ' +
+            '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> ' +
+            'in order to get a working application.'
+        }
+      });
+    }
+    const payload = {
+      workspace_id: workspace,
+      context: req.body.context || {},
+      input: req.body.input || {}
+    };
+
+    // Send the input to the conversation service
+    assistant.message(payload, (error, data) => {
+      if (error) {
+		  console.log("kkk")
+        return next(error);
+      }
+
+      return res.json(updateMessage(payload, data));
+    });
+  });
+};
---
+++ ui/js/conversation.js
@@ -0,0 +1,376 @@
+/*
+ * Copyright © 2016 I.B.M. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* The Conversation module handles the display and behavior of the chat section
+ * of the application, including the messages to and from Watson and the input box
+ */
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^Conversation$" }] */
+/* global Api: true, Common: true */
+
+
+var Conversation = (function () {
+  'use strict';
+  var ids = {
+    userInput: 'user-input',
+    userInputDummy: 'user-input-dummy',
+    chatFlow: 'chat-flow',
+    chatScrollWrapper: 'chat-scroll-wrapper'
+  };
+  var classes = {
+    messageWrapper: 'message-wrapper',
+    preBar: 'pre-bar',
+    underline: 'underline'
+  };
+  var authorTypes = {
+    user: 'user',
+    watson: 'watson'
+  };
+
+  // Publicly accessible methods defined
+  return {
+    init: init,
+    setMessage: setMessage,
+    sendMessage: sendMessage,
+    focusInput: focusInput
+  };
+
+  // Initialize Conversation module
+  function init() {
+    chatSetup();
+    initEnterSubmit();
+    setupInputBox();
+  }
+
+  // Hide chat box until there are messages,
+  // set up messages to display when user or Watson sends message
+  function chatSetup() {
+    document.getElementById(ids.chatScrollWrapper).style.display = 'none';
+
+    var currentRequestPayloadSetter = Api.setUserPayload;
+    Api.setUserPayload = function (payload) {
+      currentRequestPayloadSetter.call(Api, payload);
+      displayMessage(payload, authorTypes.user);
+    };
+
+    var currentResponsePayloadSetter = Api.setWatsonPayload;
+    Api.setWatsonPayload = function (payload) {
+      currentResponsePayloadSetter.call(Api, payload);
+      displayMessage(payload, authorTypes.watson);
+    };
+  }
+
+  // Set up the input box to submit a message when enter is pressed
+  function initEnterSubmit() {
+    document.getElementById(ids.userInput)
+      .addEventListener('keypress', function (event) {
+        if (event.keyCode === 13) {
+          sendMessage();
+          event.preventDefault();
+        }
+      }, false);
+  }
+
+  // Set up the input box to underline text as it is typed
+  // This is done by creating a hidden dummy version of the input box that
+  // is used to determine what the width of the input text should be.
+  // This value is then used to set the new width of the visible input box.
+  function setupInputBox() {
+    var input = document.getElementById(ids.userInput);
+    var dummy = document.getElementById(ids.userInputDummy);
+    var minFontSize = 9;
+    var maxFontSize = 16;
+    var minPadding = 5;
+    var maxPadding = 9;
+
+    // If no dummy input box exists, create one
+    if (dummy === null) {
+      var dummyJson = {
+        'tagName': 'div',
+        'attributes': [{
+          'name': 'id',
+          'value': (ids.userInputDummy)
+        }]
+      };
+
+      dummy = Common.buildDomElement(dummyJson);
+      document.body.appendChild(dummy);
+    }
+
+    function adjustInput() {
+      if (input.value === '') {
+        // If the input box is empty, remove the underline
+        Common.removeClass(input, 'underline');
+        input.setAttribute('style', 'width:' + '100%');
+        input.style.width = '100%';
+      } else {
+        // otherwise, adjust the dummy text to match, and then set the width of
+        // the visible input box to match it (thus extending the underline)
+        Common.addClass(input, classes.underline);
+        var txtNode = document.createTextNode(input.value);
+        ['font-size', 'font-style', 'font-weight', 'font-family', 'line-height',
+          'text-transform', 'letter-spacing'
+        ].forEach(function (index) {
+          dummy.style[index] = window.getComputedStyle(input, null).getPropertyValue(index);
+        });
+        dummy.textContent = txtNode.textContent;
+
+        var padding = 0;
+        var htmlElem = document.getElementsByTagName('html')[0];
+        var currentFontSize = parseInt(window.getComputedStyle(htmlElem, null).getPropertyValue('font-size'), 10);
+        if (currentFontSize) {
+          padding = Math.floor((currentFontSize - minFontSize) / (maxFontSize - minFontSize) *
+            (maxPadding - minPadding) + minPadding);
+        } else {
+          padding = maxPadding;
+        }
+
+        var widthValue = (dummy.offsetWidth + padding) + 'px';
+        input.setAttribute('style', 'width:' + widthValue);
+        input.style.width = widthValue;
+      }
+    }
+
+    // Any time the input changes, or the window resizes, adjust the size of the input box
+    input.addEventListener('input', adjustInput);
+    window.addEventListener('resize', adjustInput);
+
+    // Trigger the input event once to set up the input box and dummy element
+    Common.fireEvent(input, 'input');
+  }
+
+  // Retrieve the value of the input box
+  function getMessage() {
+    var userInput = document.getElementById(ids.userInput);
+    return userInput.value;
+  }
+
+  // Set the value of the input box
+  function setMessage(text) {
+    var userInput = document.getElementById(ids.userInput);
+    userInput.value = text;
+    userInput.focus();
+    Common.fireEvent(userInput, 'input');
+  }
+
+  // Send the message from the input box
+  function sendMessage(newText) {
+    var text;
+    if (newText) {
+      text = newText;
+    } else {
+      text = getMessage();
+    }
+    if (!text) {
+      return;
+    }
+    setMessage('');
+
+    Api.postConversationMessage(text);
+  }
+
+  // Display a message, given a message payload and a message type (user or Watson)
+  function displayMessage(newPayload, typeValue) {
+    var isUser = isUserMessage(typeValue);
+    var textExists = (newPayload.input && newPayload.input.text) ||
+      (newPayload.output && newPayload.output.text);
+    if (isUser !== null && textExists) {
+      if (newPayload.output && Object.prototype.toString.call(newPayload.output.text) === '[object Array]') {
+        newPayload.output.text = newPayload.output.text.filter(function (item) {
+          return item && item.length > 0;
+        }).join(' ');
+      }
+      console.log(newPayload)
+	  //console.log(newPayload.intents[0].intent === "category_spend_graph")
+      var responses = buildMessageDomElements(newPayload, isUser);
+	  //alert(2) 	
+      var chatBoxElement = document.getElementById(ids.chatFlow)
+	  if(newPayload.input.text.includes('categorical') && !isUser){
+		  //alert(1)
+		//document.getElementById(ids.chatFlow).innerHTML+="<div><a href=\"https://plot.ly/~sravanbrahma/0/?share_key=7G67USIPPMr5unoCJTueEn\" target=\"_blank\" title=\"basic-histogram\" style=\"display: block; text-align: center;\"><img src=\"https://plot.ly/~sravanbrahma/0.png?share_key=7G67USIPPMr5unoCJTueEn\" alt=\"basic-histogram\" style=\"max-width: 100%;width: 600px;\"  width=\"600\" onerror=\"this.onerror=null;this.src='https://plot.ly/404.png';\" /></a><script data-plotly=\"sravanbrahma:0\" sharekey-plotly=\"7G67USIPPMr5unoCJTueEn\" src=\"https://plot.ly/embed.js\" async></script></div>"
+		document.getElementById(ids.chatFlow).innerHTML+="<div id=\"myDiv\"><!-- Plotly chart will be drawn inside this DIV --></div>"
+		chatBoxElement = document.getElementById(ids.chatFlow);
+		var data = [{
+  values: [19, 26, 30,25],
+  labels: ['Food', 'Transport','Entertainment','Others'],
+  type: 'pie'
+}];
+
+Plotly.newPlot('myDiv', data, {}, {showSendToCloud:true});
+	  }
+
+      setResponse(responses, isUser, chatBoxElement, 0);
+    }
+  }
+
+  // Recurisive function to add responses to the chat area
+  function setResponse(responses, isUser, chatBoxElement, index) {
+    if (index < responses.length) {
+      var res = responses[index];
+      if (res.type !== 'pause') {
+        var currentDiv = getDivObject(res, isUser);
+        chatBoxElement.appendChild(currentDiv);
+        // Class to start fade in animation
+        currentDiv.classList.add('load');
+        // Move chat to the most recent messages when new messages are added
+        updateChat();
+        setResponse(responses, isUser, chatBoxElement, index + 1);
+      } else {
+        var userInputField = document.getElementById('user-input');
+        if (res.typing) {
+          userInputField.value = 'Watson Assistant Typing...';
+          console.log('watson typing ' + res.typing);
+        }
+        setTimeout(function () {
+          userInputField.value = '';
+          setResponse(responses, isUser, chatBoxElement, index + 1);
+        }, res.time);
+      }
+    }
+  }
+
+  // Constructs new DOM element from a message
+  function getDivObject(res, isUser) {
+    var messageJson = {
+      // <div class='user / watson'>
+      'tagName': 'div',
+      'classNames': ['message-wrapper', (isUser ? authorTypes.user : authorTypes.watson)],
+      'children': [{
+        // <p class='user-message / watson-message'>
+        'tagName': 'p',
+        'classNames': (isUser ? [authorTypes.user + '-message'] : [authorTypes.watson + '-message', classes.preBar]),
+        'html': (isUser ? '<img src=\'/images/head.svg\' />' + res.innerhtml : res.innerhtml)
+      }]
+    };
+    //'<img src=\'/images/head.svg\' />' + 
+    return Common.buildDomElement(messageJson);
+  }
+
+  // Determine whether a given message type is user or Watson
+  function isUserMessage(typeValue) {
+    if (typeValue === authorTypes.user) {
+      return true;
+    } else if (typeValue === authorTypes.watson) {
+      return false;
+    }
+    return null;
+  }
+
+  function getOptions(optionsList, preference) {
+    var list = '';
+    var i = 0;
+    if (preference === 'text') {
+      list = '<ul>';
+      for (i = 0; i < optionsList.length; i++) {
+        if (optionsList[i].value) {
+          list += '<li><div class="options-list" onclick="Conversation.sendMessage(\'' +
+            optionsList[i].value.input.text + '\');" >' + optionsList[i].label + '</div></li>';
+        }
+      }
+      list += '</ul>';
+    } else if (preference === 'button') {
+      list = '<br>';
+      for (i = 0; i < optionsList.length; i++) {
+        if (optionsList[i].value) {
+          var item = '<p class="option-buttons" onclick="Conversation.sendMessage(\'' + optionsList[i].value.input.text + '\');" >' + optionsList[i].label + '</p>';
+          list += item;
+        }
+      }
+    }
+    return list;
+  }
+
+  function getResponse(responses, gen) {
+    var title = '';
+
+    if (gen.hasOwnProperty('title')) {
+      title = gen.title;
+    }
+
+    if (gen.response_type === 'image') {
+      // not used in car-dashboard
+    } else if (gen.response_type === 'text') {
+      responses.push({
+        type: gen.response_type,
+        innerhtml: gen.text
+      });
+    } else if (gen.response_type === 'pause') {
+      responses.push({
+        type: gen.response_type,
+        time: gen.time,
+        typing: gen.typing
+      });
+    } else if (gen.response_type === 'option') {
+      var preference = 'text';
+      if (gen.hasOwnProperty('preference')) {
+        preference = gen.preference;
+      }
+      var list = getOptions(gen.options, preference);
+      responses.push({
+        type: gen.response_type,
+        innerhtml: title + list
+      });
+    }
+  }
+
+  // Constructs new generic elements from a message payload
+  function buildMessageDomElements(newPayload, isUser) {
+    var textArray = isUser ? newPayload.input.text : newPayload.output.text;
+    if (Object.prototype.toString.call(textArray) !== '[object Array]') {
+      textArray = [textArray];
+    }
+
+    var responses = [];
+
+    if (newPayload.hasOwnProperty('output')) {
+      if (newPayload.output.hasOwnProperty('generic')) {
+
+        var generic = newPayload.output.generic;
+
+        generic.forEach(function (gen) {
+          getResponse(responses, gen);
+        });
+      }
+    } else if (newPayload.hasOwnProperty('input')) {
+      var input = '';
+      textArray.forEach(function (msg) {
+        input += msg + ' ';
+      });
+      input.trim().replace(' ', '<br>');
+      if (input.length !== 0) {
+        responses.push({
+          type: 'text',
+          innerhtml: input
+        });
+      }
+    }
+    return responses;
+  }
+
+  // Display the chat box if it's currently hidden
+  // (i.e. if this is the first message), scroll to the bottom of the chat
+  function updateChat() {
+    //console.log('oooo');
+    document.getElementById(ids.chatScrollWrapper).style.display = '';
+    var messages = document.getElementById(ids.chatFlow).getElementsByClassName(classes.messageWrapper);
+    document.getElementById(ids.chatFlow).scrollTop = messages[messages.length - 1].offsetTop;
+  }
+
+  // Set browser focus on the input box
+  function focusInput() {
+    document.getElementById(ids.userInput).focus();
+  }
+}());
